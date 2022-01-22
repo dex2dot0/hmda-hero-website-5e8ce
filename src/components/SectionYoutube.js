9971e61fc284d3a733b3bfd1db05b25adf921443
Stackbit Code Editor <projects@stackbit.com>
2022-01-22 02:34:17 +0000

    SectionYoutube.js: changed

--- src/components/SectionYoutube.js
+++ src/components/SectionYoutube.js
@@ -5,7 +5,7 @@ import { withPrefix, markdownify } from '../utils';
 import CtaButtons from './CtaButtons';
 import useMediaQuery from "../hooks/useMediaQuery";
 
-dxport const SectionYoutube = () => {
+export const SectionYoutube = () => {
         const isDesktop = useMediaQuery('(min-width: 960px)');
         const section = _.get(this.props, 'section');
         const sectionId = _.get(section, 'section_id');
