dea0d8056e3f6cefa2b440a9794d68bc9bedd0b7
Stackbit Code Editor <projects@stackbit.com>
2022-01-22 02:50:49 +0000

    SectionYoutube.js: changed

--- src/components/SectionYoutube.js
+++ src/components/SectionYoutube.js
@@ -25,7 +25,7 @@ export default function SectionYoutube(props) {
                     }}
                 />
             )}
-            <iframe id="ytplayer" allow="fullscreen;" type="text/html" width='100%' height='720px'
+            <iframe id="ytplayer" allow="fullscreen" type="text/html" width='100%' height='720px'
             src='https://www.youtube.com/embed/uzwJ3I7pwFY?autoplay=1'
             frameborder="0" allowfullscreen>
             </iframe>
