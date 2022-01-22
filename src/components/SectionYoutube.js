3567c81eb371bdaec544259330cb163a99cdf02d
Stackbit Code Editor <projects@stackbit.com>
2022-01-22 02:07:35 +0000

    SectionYoutube.js: changed

--- src/components/SectionYoutube.js
+++ src/components/SectionYoutube.js
@@ -26,7 +26,7 @@ export default class SectionYoutube extends React.Component {
                 https://youtu.be/uzwJ3I7pwFY
                 <div className="gif-sm">
                     <iframe id="ytplayer" type="text/html" width="640" height="360"
-                    src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
+                    src="https://www.youtube.com/embed/uzwJ3I7pwFY?autoplay=1&origin=http://example.com"
                     frameborder="0">
 
                     </iframe>
